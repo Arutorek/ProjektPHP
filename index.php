04ff0e1e3f6cacb5f349fbb1eabb920ab7cff9e6
Jakub Strzelecki <arutorek@gmail.com>
2026-09-20 16:57:38 +0200

    create Teacher.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
     require_once "classes/Student.php";
+    require_once "classes/Teacher.php";
 
     $student1 = new Student("Jan", 18, "ZSK");
     $student2 = new Student("Anna", 17, "ZSŁ");
+    $teacher1 = new Teacher("Adam", 43, "Biologia");
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="/style.css">
     <title>Document</title>
 </head>
 <body>
@@ -21,12 +23,17 @@
         echo $student1->getInfo()."<br>";
         echo $student2->getInfo()."<br>";
     ?>
+    <h2>Nauczyciele</h2>
+    <?php
+        echo $teacher1->getInfo()."<br>";
+    ?>
 
     <h2>Informacje HTML - HERDOC</h2>
 
     <?php  
         echo $studnet1->getHtml();
         echo $student2->getHtml();
+        echo $teacher1->getHtml();
     ?>
 </body>
 </html>
